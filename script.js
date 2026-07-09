9ceda1f107956978cdadcbec96fa87f529d7f2b3
MarcoMdevcode <martinez3mar@gmail.com>
2026-07-08 21:57:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleSwitch = document.querySelector("#boton-tema");
+toggleSwitch.addEventListener("change", function() {
+    if (this.checked) {
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+});
